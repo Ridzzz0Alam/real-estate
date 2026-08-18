df2ac083884edab6394cfbaf40af44892343d17b
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-08-18 11:54:46 +0200

    feat:added trying to setup the signin and signup pages

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Heading,
+  useAuthenticator,
+  View,
+} from "@aws-amplify/ui-react";
+import { usePathname } from "next/navigation";
 
 Amplify.configure({
   Auth: {
@@ -13,12 +19,74 @@ Amplify.configure({
   },
 });
 
+const components = {
+  Header() {
+    return (
+      <View className="mt-4 mb-7">
+        <Heading level={3} className="!text-2xl !font-bold">
+          RENT
+          <span className="text-secondary-500 font-light hover:!text-primary-300">
+            IFUL
+          </span>
+        </Heading>
+        <p className="text-muted-foreground mt-2">
+          <span className="font-bold">Welcome!</span> Please sign in to continue
+        </p>
+      </View>
+    );
+  },
+};
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: "Enter your email",
+      label: "Email",
+      isRequired: true,
+    },
+    password: {
+      placeholder: "Enter your password",
+      label: "Password",
+      isRequired: true,
+    },
+  },
+  signUp: {
+    username: {
+      order: 1,
+      placeholder: "Choose a username",
+      label: "Username",
+      isRequired: true,
+    },
+    email: {
+      order: 2,
+      placeholder: "Enter your email address",
+      label: "Email",
+      isRequired: true,
+    },
+    password: {
+      order: 3,
+      placeholder: "Create a password",
+      label: "Password",
+      isRequired: true,
+    },
+    confirm_password: {
+      order: 4,
+      placeholder: "Confirm your password",
+      label: "Confirm Password",
+      isRequired: true,
+    },
+  },
+};
+
 const Auth = ({ children }: { children: React.ReactNode }) => {
   const { user } = useAuthenticator((context) => [context.user]);
+  const pathname = usePathname;
 
   return (
     <div className="h-full">
-      <Authenticator>{() => <>{children}</>}</Authenticator>
+      <Authenticator components={components} formFields={formFields}>
+        {() => <>{children}</>}
+      </Authenticator>
     </div>
   );
 };
